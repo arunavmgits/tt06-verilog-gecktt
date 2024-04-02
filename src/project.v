6c6e1568a546ab5365cff66b0df3cf6c126240d6
arunavmgits <163625177+arunavmgits@users.noreply.github.com>
2024-04-02 16:12:07 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -23,6 +23,50 @@ module tt_um_ha (
    // assign uio_in = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
-    assign uo_out[0] = ui_in[0] ^ ui_in[1]; // assigning sum
-    assign uo_out[1] = ui_in[0] & ui_in[1]; // assigning carry
+    reg [7:0] proc; // processing register
+reg [7:0] r1= 8'b0;
+reg [7:0] r2= 8'b0;
+reg [7:0] r3= 8'b0;
+reg [7:0] r4= 8'b0;
+reg [7:0] res;
+ 
+always @(posedge clk or posedge rst_n) begin
+    if (rst_n) begin
+        r1 <= 8'b0;
+        r2 <= 8'b0;
+        r3 <= 8'b0;
+        r4 <= 8'b0;
+        uo_out[0] <= 1'b0;
+    end else begin
+        case (uio_in)
+            2'b00: proc = r1;
+            2'b01: proc = r2;
+            2'b10: proc = r3;
+            2'b11: proc = r4;
+        endcase
+
+        if (proc == ui_in) begin
+            uo_out[0] <= 1'b0;
+        end else begin
+            if (proc > ui_in) begin
+                res = proc - ui_in;
+            end else begin
+                res = ui_in - proc;
+            end
+
+            if (res > 8'b00000010) begin
+                case (uio_in)
+                    2'b00: r1 <= ui_in;
+                    2'b01: r2 <= ui_in;
+                    2'b10: r3 <= ui_in;
+                    2'b11: r4 <= ui_in;
+                endcase
+                uo_out[0] <= 1'b1;
+            end else begin
+                uo_out[0] <= 1'b0;
+            end
+        end
+    end
+end
+
 endmodule
